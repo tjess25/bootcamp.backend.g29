0bcbbf196568ca6ac3dcadbc7ded1dddb92a6b4c
tjess25 <88819417+tjess25@users.noreply.github.com>
2023-11-24 22:01:05 -0600

    midd

--- myapp/app.js
+++ myapp/app.js
@@ -1,13 +1,12 @@
 // routes
 const userRoutes = require('./src/routes/user/index')
 
-
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+//app.use(express.urlencoded({extended: true}))
 
 /*
 const server = http.createServer((req, res) => {
@@ -18,16 +17,18 @@ const server = http.createServer((req, res) => {
 });
 */
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+/*app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-/*app.get('/user', (req, res) => {
+app.get('/user/:id', (req, res) => {
   res.status(200).send('Hello Word Get!')
 })
 
-app.post('/user', (req, res) => {
-  res.status(201).send('Hello Word Post!')
+app.post('/user', (req, res, next) => {
+  console.log("endpoint", req.body);
+  let resp = "Hello Word Post"
+  next(resp)
 })
 
 app.put('/user', (req, res) => {
@@ -38,8 +39,17 @@ app.delete('/user', (req, res) => {
   res.status(200).send('Hello Word Delete!')
 })*/
 
+app.use((req, res, next) => {
+  console.log("midd global");
+  next()
+})
+
 app.use('/user', userRoutes)
 //app.use('/product', producRoutes)
+
+app.use((resp, req, res, next) => {
+  res.status(resp.status).send(resp.send)
+})
 
 
 
--- myapp/src/routes/user/index.js
+++ myapp/src/routes/user/index.js
@@ -3,27 +3,40 @@ const router = express.Router()
 const users = './src/db/users.json'
 const fs = require('fs')
 
-router.get('/getbyid/:id',  (req, res) => {
+router.get('/getbyid/:id',  (req, res, next) => {
     const { id } = req.params
+    console.log(id);
     //const id = req.params.id
     //const name = req.params.name
     //const email = req.params.email
     fs.readFile(users, 'utf-8', (err, users) => {
-        if (err) return res.status(400).send({msg: "No se pudo abrir el archivo", err: err})
+        if (err) next({status: 400, send: {msg: "No se pudo abrir el archivo", err: err}}) 
         users = JSON.parse(users)
         
         users.forEach(u => {
             if (u.id == id) {
-                return res.send({msg: "Usuario encontrado", data: u})
+                next({status: 200, send: {msg: "Usuario encontrado", data: u}})
             }
         });
-        return res.status(404).send({msg: "Usuario no encontrado"})
+        next({status: 404, send: {msg: "Usuario no encontrado"}})
+        //return res.status(404).send({msg: "Usuario no encontrado"})
     })
 })
 
 router.get('/', (req, res) => {
     console.log(req.params); 
     res.status(200).send(`Hello Word Get Router Params ${req.params.id}!`)
+})
+
+router.delete('/', (req, res) => {
+    res.status(200).send('Hello Word Delete Router!')
+})
+
+router.use((req, res, next) => {
+    if (!req.body.password || !req.body.email) {
+      res.status(400).send({msg: "El Email y Password son requeridos"})
+    }
+    next()
 })
 
 router.post('/', (req, res) => {
@@ -35,9 +48,7 @@ router.put('/', (req, res) => {
     res.status(201).send('Hello Word Put Router!')
 })
 
-router.delete('/', (req, res) => {
-    res.status(200).send('Hello Word Delete Router!')
-})
+
 
 module.exports = router
 
